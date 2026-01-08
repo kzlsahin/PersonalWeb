bd0e0f7438567db1469573af6639f45bded85da4
Mustafa Şentürk <46689277+kzlsahin@users.noreply.github.com>
2026-01-08 20:16:41 +0300

    IdentıtyServer added [wip]

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+                new ApiScope("api1", "My API")
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            { 
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
+            };
+}
